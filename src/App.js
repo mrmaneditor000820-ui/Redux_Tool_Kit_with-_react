d0c480697d5a4a73fa7224fc5819fd88daab4ecf
mrmaneditor000820-ui <mrmaneditor000820@gmail.com>
2026-06-18 18:32:25 +0500

    conter created

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux"
+import { increment, decrement, reset } from "./counterSlice"
 
 function App() {
+
+  const count = useSelector(
+    (state) => state.counter.count
+  )
+
+  const dispatch = useDispatch()
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>{count}</h1>
+
+      <button
+        onClick={() => dispatch(increment())}>INCREMENT</button>
+
+      <button onClick={() => dispatch(decrement())} >  DECREMENT </button>
+
+      <button onClick={() => dispatch(reset())}> RESET</button>
+    </>
+  )
 }
 
-export default App;
+export default App
